5d94b6dc9e7000f3130739f4794b002d1c33abd5
Sabrina Drashen <sdrashen@gmail.com>
2023-03-28 10:35:22 -0300

    use color in an object that doesnt have a className

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,9 +1,13 @@
 import { Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+import colors from "tailwindcss/colors";
 
 export function Home() {
   return (
-    <View className="flex-1 bg-green-500 items-center justify-center">
-      <Text className="text-gray-200 text-2xl font-bold">NativeWind</Text>
+    <View className="flex-1 bg-black items-center justify-center">
+      <Feather name="home" size={34} color={colors.green[500]} />
+      <Text className="text-gray-200 text-2xl font-bold mt-4">NativeWind</Text>
     </View>
   );
 }
